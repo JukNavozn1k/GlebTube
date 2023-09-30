d413eddb650ef7c05105326ed81085371567a595
JukNavozn1k <deffender1337@gmail.com>
2023-09-30 23:12:16 +0300

    added upload form

--- GlebTube/GlebTube/urls.py
+++ GlebTube/GlebTube/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('user_auth.urls')),
+    path('',include('video_manager.urls'))
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
