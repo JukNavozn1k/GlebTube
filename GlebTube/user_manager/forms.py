ab03255974e0a13d4b1673e36b5a841207aa9f4e
JukNavozn1k <deffender1337@gmail.com>
2023-10-03 02:04:59 +0300

    more fields

--- GlebTube/user_manager/forms.py
+++ GlebTube/user_manager/forms.py
@@ -4,19 +4,24 @@ from django.contrib.auth.models import User
 class AuthForm(forms.ModelForm):    
     class Meta:
         model = User
-        fields = ['username','password',]
+        
+        fields = ['username','password','first_name','last_name','email']
         widgets = {
             
-            'username' : forms.TextInput(attrs={'class' : 'form-control','placeholder':'Моё первое видео!'}),
-            'password' : forms.PasswordInput(attrs={'class' : 'form-control'}),
+            'username' : forms.TextInput(attrs={'class' : 'form-control','placeholder':'World destroyer 2009'}),
+            'email' : forms.TextInput(attrs={'class' : 'form-control','placeholder':'example@example.com'}),
+            'password' : forms.PasswordInput(attrs={'class' : 'form-control','placeholder':'qwerty123'}),
            
         }
         labels = {
             'username' : 'Имя пользователя',
             'password' : 'Пароль',
+            'first_name' : 'Имя',
+            'last_name' : 'Фамилия'
           
         }
         help_texts = {
             'username': 'Имя пользователя должно быть уникальным и не может быть измененено',
-            'password' : None
+            'password' : None,
+            
         }
