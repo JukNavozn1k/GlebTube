656f5a8852e4e50ed86a31efe37432da54e0c2b0
JukNavozn1k <deffender1337@gmail.com>
2023-10-03 23:34:39 +0300

    async request (no ajax) and count of likes dislices (static)

--- GlebTube/video_manager/static/js/buttons.js
+++ GlebTube/video_manager/static/js/buttons.js
@@ -1,21 +1,33 @@
-var btn1 = document.querySelector('#green');
-var btn2 = document.querySelector('#red');
+document.querySelectorAll(".post").forEach(post => {
+	const postId = post.dataset.postId;
+	const ratings = post.querySelectorAll(".post-rating");
+	const likeRating = ratings[0];
 
-btn1.addEventListener('click', function() {
-  
-    if (btn2.classList.contains('red')) {
-      btn2.classList.remove('red');
-    } 
-  this.classList.toggle('green');
-  
+	ratings.forEach(rating => {
+		const button = rating.querySelector(".post-rating-button");
+		const count = rating.querySelector(".post-rating-count");
+
+		button.addEventListener("click", async () => {
+			if (rating.classList.contains("post-rating-selected")) {
+				return;
+			}
+
+			count.textContent = Number(count.textContent) + 1;
+
+			ratings.forEach(rating => {
+				if (rating.classList.contains("post-rating-selected")) {
+					const count = rating.querySelector(".post-rating-count");
+
+					count.textContent = Math.max(0, Number(count.textContent) - 1);
+					rating.classList.remove("post-rating-selected");
+				}
+			});
+
+			rating.classList.add("post-rating-selected");
+
+			const likeOrDislike = likeRating === rating ? "like" : "dislike";
+			const response = await fetch(`/posts/${postId}/${likeOrDislike}`);
+			const body = await response.json();
+		});
+	});
 });
-
-btn2.addEventListener('click', function() {
-  
-    if (btn1.classList.contains('green')) {
-      btn1.classList.remove('green');
-    } 
-  this.classList.toggle('red');
-  
-});
-
