81bbb90c8ebe85a4872d193365583024c1a7a987
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-10-29 23:32:51 +0300

    unrate video

--- GlebTube/video_manager/static/js/buttons.js
+++ GlebTube/video_manager/static/js/buttons.js
@@ -1,19 +1,19 @@
 document.querySelectorAll(".post").forEach(post => {
+	
 	const postId = post.dataset.postId;
 	const ratings = post.querySelectorAll(".post-rating");
 	const likeRating = ratings[0];
-
+	
 	ratings.forEach(rating => {
 		const button = rating.querySelector(".post-rating-button");
 		const count = rating.querySelector(".post-rating-count");
 		
 		button.addEventListener("click", async () => {
+			var unrate = false;
 			if (rating.classList.contains("post-rating-selected")) {
-				
-				return;
+				unrate = true;
 			}
-
-			count.textContent = Number(count.textContent) + 1;
+			
 
 			ratings.forEach(rating => {
 				if (rating.classList.contains("post-rating-selected")) {
@@ -23,10 +23,13 @@ document.querySelectorAll(".post").forEach(post => {
 					rating.classList.remove("post-rating-selected");
 				}
 			});
-
-			rating.classList.add("post-rating-selected");
-
-			const likeOrDislike = likeRating === rating ? "like" : "dislike";
+			if (!unrate) 
+			{
+				count.textContent = Number(count.textContent) + 1;
+				rating.classList.add("post-rating-selected");
+				var likeOrDislike = likeRating === rating ? "like" : "dislike";	
+			}
+			else {likeOrDislike = "unrate"}
 			const response = await fetch(`/video/${postId}/${likeOrDislike}`);
 			const body = await response.json();
 		});
