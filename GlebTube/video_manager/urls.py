9e007fc11b64c9193fe34318018caaad17ff5a86
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-10-30 16:19:49 +0300

    video -> rate_video (need to make same with coments)

--- GlebTube/video_manager/urls.py
+++ GlebTube/video_manager/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path('upload/',views.Upload.as_view()),
   path('watch/<int:video_id>',views.Watch.as_view()),
-  path('video/<int:video_id>/<path:action>',views.video)
+  path('rate_video/<int:video_id>/<path:action>',views.rate_video),
 ] 
