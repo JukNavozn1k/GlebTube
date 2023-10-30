6e310d1e94d96c6b961b8d3604dfa8b64e9f2f47
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-10-30 20:03:32 +0300

    removed unused func

--- GlebTube/video_manager/static/js/buttons.js
+++ GlebTube/video_manager/static/js/buttons.js
@@ -1,20 +1,3 @@
-function parseVideoIdFromUrl() {
-    const url = window.location.pathname; // Get the current URL
-    const parts = url.split('/'); // Split the URL by '/'
-    
-    // Assuming 'video_id' is the second part in the URL (index 2, considering 0-based indexing)
-    const videoId = parseInt(parts[2], 10);
-
-    if (!isNaN(videoId)) {
-        // videoId contains the parsed integer value
-        return videoId;
-    } else {
-        // Handle the case where 'video_id' is not a valid integer
-        return null;
-    }
-}
-
-
  // Get the CSRF token using the {% csrf_token %} template tag
 const csrfToken = document.querySelector('input[name=csrfmiddlewaretoken]').value;
 
