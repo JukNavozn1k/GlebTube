99b3e87ae98b1f9052482ff2a581d0c79cea0dab
JukNavozn1k <deffender1337@gmail.com>
2023-11-01 03:44:53 +0300

    seek bug fix + autoplay

--- GlebTube/video_manager/static/js/player.js
+++ GlebTube/video_manager/static/js/player.js
@@ -1,1 +1,10 @@
-const player = new Plyr('#player');
+var player = new Plyr('#player', {
+    autoplay: true
+  });
+  
+player.on('ready', (event) => {
+    setTimeout(() => {
+    let startTime = store.get('plyr-time-audio-01'); 
+        if (startTime > 0) player.seek(startTime);
+}, 200);
+});
