c1f2bd0f57fb9e6c424866189e9a5a4fec8c2b64
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-06 00:10:52 +0300

    added history

--- GlebTube/video_manager/urls.py
+++ GlebTube/video_manager/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
   path('upload/',views.Upload.as_view()),
+  path('history/',views.History.as_view()),
   path('watch/<int:video_id>',views.Watch.as_view()),
   path('watch/<int:video_id>/<path:action>',views.Watch.as_view()),
   
