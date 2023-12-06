46086d157ba4e6fec96d9ef040c45311954e73c7
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-07 01:51:46 +0300

    History model (video_manager -> user_manager)

--- GlebTube/user_manager/models.py
+++ GlebTube/user_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from video_manager.models import Video
+
 # Create your models here.
+class History(models.Model):
+      viewer = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+      video = models.ForeignKey(Video,null=True,on_delete=models.CASCADE)
+      class Meta:
+        verbose_name = 'Просмотр'
+        verbose_name_plural = 'История просмотров'
