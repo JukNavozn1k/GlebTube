4beb4e99543723dce60c05144128f46a59b0d1d8
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-07 03:06:35 +0300

    added edit form

--- GlebTube/video_manager/urls.py
+++ GlebTube/video_manager/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   path('upload/',views.UploadVideo.as_view()),
-  # path('edit/video/<int:video_id>',views.EditVideo.as_view()),
+  path('edit/video/<int:video_id>',views.EditVideo.as_view()),
   path('history/',views.History.as_view()),
   path('my_videos/',views.my_videos),
   path('delete/video/<int:video_id>',views.delete_video),
