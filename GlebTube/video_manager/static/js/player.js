cc8d46ce10dc79d3fd49eb5ed096d4e04c0e647a
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-11 01:45:52 +0300

    plyr error fixed

--- GlebTube/video_manager/static/js/player.js
+++ GlebTube/video_manager/static/js/player.js
@@ -2,9 +2,3 @@ var player = new Plyr('#player', {
     autoplay: true
   });
   
-player.on('ready', (event) => {
-    setTimeout(() => {
-    let startTime = store.get('plyr-time-audio-01'); 
-        if (startTime > 0) player.seek(startTime);
-}, 200);
-});
