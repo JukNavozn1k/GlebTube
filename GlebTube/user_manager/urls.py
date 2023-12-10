831fd92be296fb40c4e78330f63e2cf8d1e9c393
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-10 21:16:26 +0300

    added working user profile

--- GlebTube/user_manager/urls.py
+++ GlebTube/user_manager/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
    path('login/',views.Login.as_view()),
    path('reg/',views.Reg.as_view()),
    path('logout/', views.Logout.as_view()),
-   path('profile/',views.Profile.as_view()),
+   path('profile/<path:user>',views.Profile.as_view()),
 ]
