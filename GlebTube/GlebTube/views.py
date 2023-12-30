f1e4e4a49b6454e5ab453da651d01f63007b7389
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-30 05:24:43 +0300

    added footer / empty page (not ready)

--- GlebTube/GlebTube/views.py
+++ GlebTube/GlebTube/views.py
@@ -6,7 +6,7 @@ from video_manager import models
 
 def home(request):
     videos = models.Video.objects.all()
-    context = {'videos': videos}
+    context = {'videos': videos, 'title':'Главная'}
     return render(request,'main.html',context=context)
 
 
