f86c26de7f667100864f39d4716710b992a236ac
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-31 04:47:28 +0300

    username : str -> username : User

--- GlebTube/user_manager/views.py
+++ GlebTube/user_manager/views.py
@@ -64,7 +64,7 @@ class Profile(views.View):
             if request.user == user: isOwner = True
             
             likes_count = RateVideo.objects.all().filter(grade=1,content__in = Video.objects.filter(author=user)).count()
-            context = {'username': user.username,'isOwner':isOwner,'likes_count':likes_count}
+            context = {'user': user,'isOwner':isOwner,'likes_count':likes_count}
             return render(request,'profile.html',context=context)
         except User.DoesNotExist: 
             raise Http404("The requested resource was not found.")
