4d1a1bcad60983442b4df65b17056e1452eaef37
JukNavozn1k <86569510+JukNavozn1k@users.noreply.github.com>
2023-12-31 03:46:10 +0300

    added more comments on user model's

--- GlebTube/user_manager/models.py
+++ GlebTube/user_manager/models.py
@@ -9,8 +9,10 @@ from django.db.models.signals import post_save
 
 from django.core.exceptions import ValidationError
 
-# Create your models here.
 class History(models.Model):
+      '''
+          Allows you to record your watch history.
+      '''
       viewer = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
       video = models.ForeignKey(Video,null=True,on_delete=models.CASCADE)
       class Meta:
@@ -18,24 +20,33 @@ class History(models.Model):
         verbose_name_plural = 'История просмотров'
   
 class UserAdditional(models.Model):
+      '''
+            Allows you to augment the user entity with additional attributes 
+            without modifying the standard User class
+      '''
       user = models.OneToOneField(User,unique=True,on_delete=models.CASCADE)
       profile_description = models.TextField(max_length=1024,default='Здесь будет замечательное описание, когда-нибудь... ')
       avatar = models.ImageField(upload_to='user_avatars',default='user_avatars/default.png')
 
 
+
 class Subscription(models.Model):
+      '''
+            Allows you to register subscriptions. 
+            Unsubscription is performed by deleting records
+      '''
       subscriber = models.ForeignKey(User,unique=True,on_delete=models.CASCADE,related_name='subscriber')
       author = models.ForeignKey(User,unique=True,on_delete=models.CASCADE,related_name='author')
       class Meta:
             constraints = [
                   models.UniqueConstraint(fields=['subscriber', 'author'], name='unique_users')
             ]
-
       def clean(self):
         if self.author == self.subscriber:
             raise ValidationError("User and linked user cannot be the same.")
 
-# auto creating additional model connected to user
+
+# Automatically binds User Additional proxy to User
 @receiver(post_save, sender=User)
 def create_additional(sender, instance, created, **kwargs):
     if created:
