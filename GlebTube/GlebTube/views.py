e98728e81cdb7b405b1a38e8e81566eaf3f5e3ee
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-09 21:24:47 +0300

    my vids, main n+1 fix

--- GlebTube/GlebTube/views.py
+++ GlebTube/GlebTube/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from video_manager import models
 
 def home(request):
-    videos = models.Video.objects.all()
+    videos = models.Video.objects.select_related('author')
     context = {'videos': videos, 'title':'Главная'}
     return render(request,'main.html',context=context)
 
