033385660dae25d091e068db96d5d77374defca1
JukNavozn1k <JukNavozn1k>
2024-07-10 23:31:22 +0300

    rm comments

--- GlebTube/video_manager/static/js/comment_video.js
+++ GlebTube/video_manager/static/js/comment_video.js
@@ -1,17 +1,6 @@
 
-const commentsCountElement = document.getElementById("comments_count");
-const  commentListDiv = document.querySelector('.comment_list');
-
-
 // Clear comment
 document.getElementById('clearButton').addEventListener('click', function () {
     comment = document.getElementById('comment').value = "";
 });
 
-document.body.addEventListener('htmx:afterRequest', function (evt) {
- 
-  if (evt.detail.xhr.status === 401) {
-    alert("Ошибка: Необходима авторизация.");
-  }
-  else {alert("Ошибка: Мы стараемся её исправить...");}
-});
