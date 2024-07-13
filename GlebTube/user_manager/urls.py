68bf8f0893012f38761e318ff18faa74aa890dcf
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-14 01:59:08 +0300

    username -> userid (profile)

--- GlebTube/user_manager/urls.py
+++ GlebTube/user_manager/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
    path('reg/',views.Reg.as_view()),
    path('logout/', views.Logout.as_view()),
 
-   path('profile/<path:user>',views.Profile.as_view()),
+   path('profile/<int:user>',views.Profile.as_view()),
 
-   path('profile_action/<path:user>/videos',views.UserVideos.as_view()),
-   path('profile_action/<path:user>/liked_videos',views.UserLiked.as_view()),
-   path('profile_action/<path:user>/subscribe',views.Subscribe.as_view()),
+   path('profile_action/<int:user>/videos',views.UserVideos.as_view()),
+   path('profile_action/<int:user>/liked_videos',views.UserLiked.as_view()),
+   path('profile_action/<int:user>/subscribe',views.Subscribe.as_view()),
    path('accounts/profile/', home),
    path('delete/history',views.delete_history)
 ]
