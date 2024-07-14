a3456826be2a512e33bb7e4c73c17adb1c2c9d78
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-15 01:45:29 +0300

    .env variables  (docker-compose)

--- GlebTube/video_manager/tasks.py
+++ GlebTube/video_manager/tasks.py
@@ -1,13 +1,10 @@
 from celery import shared_task
-from .models import Video,RateVideo
-from user_manager.models import History
+from .models import Video
 
 # inc view count and adds to hist if user is authenticated
 @shared_task
-def statVideo(video_id,user_id=None):
-    video = Video.objects.get(Video,id=video_id)
+def inc_views(video_id):
+    video = Video.objects.get(id=video_id)
     video.views += 1
-    video.stars_count = RateVideo.objects.filter(content=video,grade=1).count()
     video.save()
-    if not (user_id is None): 
-            History.objects.create(viewer__id=user_id,video=video)
+ 
