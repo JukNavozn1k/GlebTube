d697516d9b7c048bd8bf6b046c98ef41960f20dc
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-14 04:11:13 +0300

    no n+1 and joins (video list query)

--- GlebTube/GlebTube/views.py
+++ GlebTube/GlebTube/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from video_manager import models
 
 def home(request):
-    videos = models.Video.objects.select_related('author')
+    videos = models.Video.objects.all()
     context = {'videos': videos, 'title':'Главная'}
     return render(request,'main.html',context=context)
 
