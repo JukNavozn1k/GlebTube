ee0da8e27f558212ea7ca0ee079639552b740382
JukNavozn1k <JukNavozn1k>
2024-07-14 12:43:53 +0300

    celery init

---
+++ GlebTube/celery_app.py
@@ -0,0 +1,16 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','GlebTube.settings')
+
+app = Celery('GlebTube')
+
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+@app.task()
+def hello_celery():
+    print('Hello, Celery!')
+    
