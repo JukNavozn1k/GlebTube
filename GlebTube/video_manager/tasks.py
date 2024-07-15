6801baf778fb6b88006f66a3305fded8344256a7
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-15 05:45:12 +0300

    viewed = 1 bugfix

--- GlebTube/video_manager/tasks.py
+++ GlebTube/video_manager/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 @shared_task
 def create_user_video_relation(video_id,user_id):
     from .models import UserVideoRelation
-    UserVideoRelation.objects.get_or_create(video__id=video_id,user__id=user_id,viewed=1)
+    UserVideoRelation.objects.get_or_create(video__id=video_id,user__id=user_id)
     refresh_stats.delay(video_id)
 
 # updates views & stars count models.Video
@@ -14,3 +14,4 @@ def refresh_stats(video_id):
     video.views = UserVideoRelation.objects.filter(video__id=video_id).count()
     video.stars_count = UserVideoRelation.objects.filter(grade=1,video__id=video_id).count()
     video.save()
+
