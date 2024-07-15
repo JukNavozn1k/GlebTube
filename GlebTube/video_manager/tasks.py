8de44ad29f5059307ac17734eec641bcac965ad6
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-15 03:47:15 +0300

    get or create Relation on watch

--- GlebTube/video_manager/tasks.py
+++ GlebTube/video_manager/tasks.py
@@ -1,8 +1,7 @@
 from celery import shared_task
 
-from django.db.models import F
 
-# inc view count and adds to hist if user is authenticated
+# updates views & stars count models.Video
 @shared_task
 def refresh_stats(video_id):
     from .models import Video,UserVideoRelation
@@ -10,4 +9,3 @@ def refresh_stats(video_id):
     video.views = UserVideoRelation.objects.filter(video__id=video_id).count()
     video.stars_count = UserVideoRelation.objects.filter(grade=1,video__id=video_id).count()
     video.save()
- 
