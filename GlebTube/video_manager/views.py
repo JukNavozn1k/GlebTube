b2dd0874631332c97a6541c94375ab6a00673573
JukNavozn1k <JukNavozn1k>
2024-07-15 17:08:31 +0300

    hls working, but not

--- GlebTube/video_manager/views.py
+++ GlebTube/video_manager/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,redirect,HttpResponse
+from django.shortcuts import render,redirect
+from django.http import FileResponse, HttpResponse
 
 from django.views import View
 
@@ -8,6 +9,47 @@ from . import models,forms
 from .models import Video
 
 from . import tasks
+
+import os 
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import FileResponse, HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Video
+
+
+def serve_hls_playlist(request, video_id):
+    try:
+        video = get_object_or_404(Video, pk=video_id)
+        hls_playlist_path = video.hls
+
+        with open(hls_playlist_path, 'r') as m3u8_file:
+            m3u8_content = m3u8_file.read()
+
+        base_url = request.build_absolute_uri('/') 
+        serve_hls_segment_url = base_url +"serve_hls_segment/" +str(video_id)
+        m3u8_content = m3u8_content.replace('{{ dynamic_path }}', serve_hls_segment_url)
+
+
+        return HttpResponse(m3u8_content, content_type='application/vnd.apple.mpegurl')
+    except (Video.DoesNotExist, FileNotFoundError):
+        return HttpResponse("Video or HLS playlist not found", status=404)
+
+
+def serve_hls_segment(request, video_id, segment_name):
+    try:
+        video = get_object_or_404(Video, pk=video_id)
+        hls_directory = os.path.join(os.path.dirname(video.video.path), 'hls_output')
+        segment_path = os.path.join(hls_directory, segment_name)
+
+        # Serve the HLS segment as a binary file response
+        return FileResponse(open(segment_path, 'rb'))
+    except (Video.DoesNotExist, FileNotFoundError):
+        return HttpResponse("Video or HLS segment not found", status=404)
+
+
+
+
 
 
 
@@ -49,7 +91,9 @@ class VideoView(View):
         video = get_object_or_404(Video,id=video_id)
         if request.user.is_authenticated: 
             tasks.refresh_history.delay(video.id,request.user.id)
-        context = {'video':video} 
+
+        hls_playlist_url = reverse('serve_hls_playlist', args=[video.id])
+        context = {'video':video,'hls_url': hls_playlist_url} 
         tasks.refresh_views.delay(video_id)
         return render(request,'watch.html',context=context)
     def delete(self,request,video_id):
