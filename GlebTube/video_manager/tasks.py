47894b74f5caaf097e5e8fe1588a5d852df31d9f
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-15 03:30:22 +0300

    fixed import bug (celery)

--- GlebTube/video_manager/tasks.py
+++ GlebTube/video_manager/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-from .models import Video,UserVideoRelation
 
 from django.db.models import F
 
 # inc view count and adds to hist if user is authenticated
 @shared_task
 def refresh_stats(video_id):
+    from .models import Video,UserVideoRelation
     video = Video.objects.get(id=video_id)
     video.views = UserVideoRelation.objects.filter(video__id=video_id).count()
     video.stars_count = UserVideoRelation.objects.filter(grade=1,video__id=video_id).count()
