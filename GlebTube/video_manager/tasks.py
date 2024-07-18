47bb2473f036ed4af31bdc8abe71029b6061046f
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-19 02:30:22 +0300

    rip celery (post comment)

--- GlebTube/video_manager/tasks.py
+++ GlebTube/video_manager/tasks.py
@@ -27,12 +27,6 @@ def remove_comment(comment_id,author_id):
     CommentVideo.objects.filter(id=comment_id,author__id = author_id).delete()
 
 @shared_task
-def post_comment(video_id,author_id,comment):
-    from .models import CommentVideo
-    new_comment = CommentVideo(author_id=author_id,instance_id=video_id,content=comment)
-    new_comment.save()
-
-@shared_task
 def update_video_rate(video_id,author_id):
     from .models import UserVideoRelation
     user_video_relation = UserVideoRelation.objects.get(video_id=video_id, user_id=author_id)
