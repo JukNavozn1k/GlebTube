be3b4c0ded1e16c99636ec48d179421e7cd3c65c
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-19 01:17:12 +0300

    4 query sub

--- GlebTube/user_manager/views.py
+++ GlebTube/user_manager/views.py
@@ -102,24 +102,24 @@ class UserLiked(views.View):
 
 
 class Subscribe(views.View):
+    def get_response_data(self,request,context,active):
+        if active:
+            return render(request,'sub_buttons/unsub.html',context=context)
+        return render(request,'sub_buttons/sub.html',context=context)
+
     def get(self,request,user):
         user = get_object_or_404(User,id=user)
         if request.user.is_authenticated:
             subscription,created = models.Subscription.objects.get_or_create(subscriber = request.user,author=user)
             context = {'user' : user}
-            if subscription.active:
-                return render(request,'sub_buttons/unsub.html',context=context)
-            else: 
-                return render(request,'sub_buttons/sub.html',context=context)
+            return self.get_response_data(request,context,subscription.active)
         return HttpResponse("",status=401)
     def put(self,request,user):
         if request.user.is_authenticated:
             user = get_object_or_404(User,id=user)
             subscription,created = models.Subscription.objects.get_or_create(subscriber = request.user,author=user)
-            if not created:
-                subscription.active = not subscription.active
-                subscription.save()
-            return self.get(request,user.id)
+            tasks.update_subscription.delay(request.user.id,user.id)
+            return self.get_response_data(request,{'user':user},not subscription.active)
         return HttpResponse("",status=401)
 
 
