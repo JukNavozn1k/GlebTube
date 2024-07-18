c15f87f4f0db0d010f0f2614c55c2387b987db8a
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-19 02:06:42 +0300

    Rm redis cache

--- GlebTube/GlebTube/views.py
+++ GlebTube/GlebTube/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
 from video_manager import models
 
-from django.conf import settings
-from django.core.cache import cache
 
 def home(request):
-    videos = cache.get(settings.CACHE_ALL_VIDEO_QUERYSET)
-    if not videos:
-       videos = models.Video.objects.all()
-       cache.set(settings.CACHE_ALL_VIDEO_QUERYSET,videos,timeout=settings.CACHE_ALL_VIDEO_QUERYSET_TIMEOUT)
-    
-    context = {'videos': videos, 'title':'Главная'}
-    return render(request,'main.html',context=context)
+  videos = models.Video.objects.all()
+  context = {'videos': videos, 'title':'Главная'}
+  return render(request,'main.html',context=context)
 
 
 def handler404(request, *args, **argv):
