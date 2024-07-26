4f3bd3d609f92b86368cdfde9afb01cca33a13b4
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-26 19:15:05 +0300

    hls moved into root

--- GlebTube/GlebTube/urls.py
+++ GlebTube/GlebTube/urls.py
@@ -33,6 +33,9 @@ urlpatterns = [
     path('',include('watch.urls')),
     path('', include('social_django.urls', namespace='social')),
     path('accounts/profile/', views.home),
+    
+    path('serve_hls_playlist/<int:video_id>/', views.serve_hls_playlist, name='serve_hls_playlist'),
+    path('serve_hls_segment/<int:video_id>/<str:segment_name>/',views.serve_hls_segment, name='serve_hls_segment'),
 ] 
 
 if DEBUG:
