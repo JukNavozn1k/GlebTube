f27d8d50552e73e31a7fbd4764f40497de33c5e3
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-27 16:08:31 +0300

    loaded user & additional (4 requests)

--- GlebTube/profiles/models.py
+++ GlebTube/profiles/models.py
@@ -27,6 +27,9 @@ class UserAdditional(models.Model):
       profile_description = models.TextField(max_length=1024,verbose_name='Описание',blank=True,null=True)
       avatar = models.ImageField(upload_to='user_avatars',verbose_name='Аватар',null=True,blank=True)
 
+      stars_count = models.PositiveBigIntegerField(default=0, verbose_name='Количество звёзд')
+      subs_count = models.PositiveBigIntegerField(default=0, verbose_name='Количество подписчиков')
+      
       class Meta: 
           verbose_name = 'Дополнение к пользователю'
           verbose_name_plural = 'Дополнения к пользователям'
