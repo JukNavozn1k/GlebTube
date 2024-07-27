06cfb44d8fecefd9fcf054ee11a0db30f1287a18
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-27 16:36:00 +0300

    counting subs via celery

--- GlebTube/profiles/tasks.py
+++ GlebTube/profiles/tasks.py
@@ -11,3 +11,13 @@ def update_subscription(user_id,author_id):
     sub = Subscription.objects.get(subscriber_id=user_id,author_id=author_id)
     sub.active = not sub.active
     sub.save()
+    refresh_subs_count.delay(author_id)
+    
+    
+@shared_task
+def refresh_subs_count(author_id):
+    from .models import Subscription,UserAdditional
+    additional,created = UserAdditional.objects.get_or_create(user_id=author_id)
+    additional.subs_count = Subscription.objects.filter(author_id=author_id,active=True).count()
+    additional.save()
+    
