9917c257b059f7a2304c42a5b19682599d80c9eb
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-07-27 20:36:00 +0300

    htmx fideo fetch removed (-2 sql requests)

--- GlebTube/watch/urls.py
+++ GlebTube/watch/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
   path('add_comment_video/<int:video_id>',views.CommentVideo.as_view(),name='add_comment'),
   path('rm_comment/<int:comment_id>/',views.CommentVideo.as_view(),name='delete_comment'),
   
-  path('fetch/video/<int:video_id>/',views.VideoPlayer.as_view(),name='fetch_video'),
   path('fetch/comments/<int:video_id>/',views.CommentVideo.as_view(),name='fetch_comments'),
   path('rate_video/<int:video_id>/',views.RateVideoView.as_view(),name='rate_video'),
 
