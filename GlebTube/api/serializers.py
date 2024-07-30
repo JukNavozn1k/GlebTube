09aaa3cf01693c23bd52fa9f07c533052adf5f8e
PavelSilinskiy <pavel.silinskiy.03@mail.ru>
2024-07-30 12:10:02 +0300

    хз

--- GlebTube/api/serializers.py
+++ GlebTube/api/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework.serializers import *
-import video_manager.models as md
+import videos.models as md
 
-class VideoSerializer(ModelSerializer):
+class VideoApiSerializer(ModelSerializer):
     class Meta:
         model = md.Video
         exclude = ('video', )
         # fields = ('id', 'caption', 'description', 'thumbnail', 'duration', 'hls', 
         #           'status', 'is_running', 'views', 'stars_count', 'date_uploaded', 'author')
 
-class CommentsSerializer(ModelSerializer):
+class CommentsApiSerializer(ModelSerializer):
     class Meta:
         model = md.Video
+
         exclude = ()
     
         
