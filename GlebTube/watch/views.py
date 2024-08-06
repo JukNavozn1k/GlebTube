6436683a2767d4ce10ad77e15a86af36e2c28001
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-06 17:05:23 +0300

    registered model

--- GlebTube/watch/views.py
+++ GlebTube/watch/views.py
@@ -32,6 +32,7 @@ class VideoView(View):
 
 class CommentVideo(View):
     def get(self,request,video_id):
+        print(User.objects.all().select_related('user_comments_rates'))
         users_with_additional =  Prefetch('author',User.objects.all().select_related('additional'))
         comments = models.CommentVideo.objects.all().filter(instance__id=video_id).order_by('-id').prefetch_related(
            users_with_additional)
