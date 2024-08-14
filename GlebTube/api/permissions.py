b5c1bc598ceb8ad301b7f4059cf3f2b4f2506817
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-14 21:21:52 +0300

    obj permission

--- GlebTube/api/permissions.py
+++ GlebTube/api/permissions.py
@@ -2,4 +2,4 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class EditUserPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool((request.user.is_authenticated and request.user == obj) or request.method in SAFE_METHODS)
+        return bool((request.user.is_authenticated and (request.user == obj or request.user.is_staff)) or request.method in SAFE_METHODS)
