b426694a4a05c66b17193fde94d1a3db38911376
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-14 20:15:03 +0300

    api view (Video, Comment, User)

--- GlebTube/api/urls.py
+++ GlebTube/api/urls.py
@@ -3,5 +3,6 @@ import api.views as views
 
 smp = SimpleRouter()
 smp.register('video', views.VideoApiView)
-
+smp.register('user', views.UserApiView)
+smp.register('comment', views.CommentsApiView)
 urlpatterns = smp.urls
--- GlebTube/api/views.py
+++ GlebTube/api/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from rest_framework.viewsets import *
-from videos.models import Video
+from rest_framework import mixins
+
+from videos.models import Video,CommentVideo
+from auths.models import User
 from api.serializers import *
 # Create your views here.
 
+from django.db.models import Prefetch
 
-class VideoApiView(ModelViewSet):
-    queryset = Video.objects.all()
+class UserApiView(mixins.RetrieveModelMixin,mixins.ListModelMixin,GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+
+class CommentsApiView(mixins.RetrieveModelMixin,mixins.ListModelMixin,GenericViewSet):
+    queryset = CommentVideo.objects.all().prefetch_related('author')
+    serializer_class = CommentSerializer
+    
+
+
+class VideoApiView(mixins.RetrieveModelMixin,mixins.ListModelMixin,GenericViewSet):
+
+    queryset = Video.objects.all().select_related('author').prefetch_related('video_comments__author')
     serializer_class = VideoApiSerializer
+    
 
