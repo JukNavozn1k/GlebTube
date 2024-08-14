0c8e78f4de8063056df314eea6d3696e0807ae39
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-14 20:15:03 +0300

    api view (Video, Comment, User)

--- GlebTube/api/serializers.py
+++ GlebTube/api/serializers.py
@@ -1,18 +1,28 @@
 from rest_framework.serializers import *
-import videos.models as md
+import videos.models as video_models
+from auths.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "avatar", "profile_description")
+
+
+class CommentSerializer(ModelSerializer):
+    author = UserSerializer()
+    class Meta:
+        model = video_models.CommentVideo
+        fields = "__all__"
+
 
 
 class VideoApiSerializer(ModelSerializer):
+    
+    author = UserSerializer()
+    video_comments = CommentSerializer(many=True)
     class Meta:
-        model = md.Video
+        model = video_models.Video
         fields = '__all__'
 
 
-
-class CommentsApiSerializer(ModelSerializer):
-    class Meta:
-        model = md.Video
-
-        exclude = ()
-    
-        
