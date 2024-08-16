a07abd85094b65d9653649ded28def670071b8e7
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-16 18:45:15 +0300

    fixed sub button

--- GlebTube/profiles/urls.py
+++ GlebTube/profiles/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     path('profile_action/<int:user>/videos',views.UserVideos.as_view(),name='user_videos'),
     path('profile_action/<int:user>/liked_videos',views.UserLiked.as_view(),name='user_liked'),
-    path('profile_action/<int:user>/subscribe',views.SubscribeView.as_view(),name='subscribe'),
+    path('profile_action/<int:user>/subscribe',views.SubscribeButtonView.as_view(),name='subscribe_button'),
     path('profile_action/<int:user>/history',views.History.as_view(),name='history'),
 
     path('profile/edit/',views.ProfileEdit.as_view(),name='edit_profile'),
