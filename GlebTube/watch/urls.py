a80ae602c7ce6a72c2f5de90e43a9c2d10aec2f8
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-08-17 16:19:00 +0300

    sub button fix

--- GlebTube/watch/urls.py
+++ GlebTube/watch/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
   
   path('rate_video/<int:video_id>/',views.RateVideoView.as_view(),name='rate_video'),
   path('rate_comment/<int:comment_id>/',views.RateCommentView.as_view(),name='rate_comment'),
-  path('subscribe/<int:user_id>/',views.SubscribeBtnView.as_view(),name='subscribe'),
+
 
   
 ] 
