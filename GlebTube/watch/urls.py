a473dd1aff8b7a103fb67392c0e0b1d960351299
gleb <86569510+JukNavozn1k@users.noreply.github.com>
2024-09-15 14:33:42 +0300

    deleted comment.html & moved crud ops into edit comment view

--- GlebTube/watch/urls.py
+++ GlebTube/watch/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
   
   path('video/<int:video_id>',views.VideoView.as_view(),name='video'),
   
-  path('add_comment_video/<int:video_id>',views.ViewComments.as_view(),name='add_comment'),
-  path('rm_comment/<int:comment_id>/',views.ViewComments.as_view(),name='delete_comment'),
+  path('add_comment_video/<int:video_id>',views.EditComment.as_view(),name='add_comment'),
+  path('rm_comment/<int:comment_id>/',views.EditComment.as_view(),name='delete_comment'),
   
   path('fetch/comments/<int:video_id>/',views.ViewComments.as_view(),name='fetch_comments'),
   path('fetch/comment/<int:comment_id>/',views.ViewComment.as_view(),name='fetch_comment'),
