08bcd35f43c161ca196a8108acd5fdf741ed6de5
JukNavozn1k <JukNavozn1k>
2024-09-25 00:10:38 +0300

    default web token auth

--- GlebTube/GlebTube/settings.py
+++ GlebTube/GlebTube/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+  
     'drf_yasg',
     'djoser',
     
@@ -203,6 +204,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
          # 'rest_framework.authentication.TokenAuthentication',
          # 'rest_framework.authentication.BasicAuthentication',
     ]
