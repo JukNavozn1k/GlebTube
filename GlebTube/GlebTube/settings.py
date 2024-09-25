4a06ec76dc455bd2b2cdad0d6a73da8eb4d47b7a
JukNavozn1k <JukNavozn1k>
2024-09-25 13:28:46 +0300

    fixed static media roots

--- GlebTube/GlebTube/settings.py
+++ GlebTube/GlebTube/settings.py
@@ -40,10 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-  
     'drf_yasg',
     'djoser',
-    
     'social_django',
     'auths',
     'profiles',
@@ -139,9 +137,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR,'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
