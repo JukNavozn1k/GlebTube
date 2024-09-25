2e0dcce5d1b60bc561fcf51354c9550ea79ee87e
JukNavozn1k <JukNavozn1k>
2024-09-25 12:56:02 +0300

    fixed static root

--- GlebTube/GlebTube/settings.py
+++ GlebTube/GlebTube/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 if DEBUG:    
-    STATICFILES_DIRS = [BASE_DIR / "static"] 
+    STATIC_ROOT = os.path.join(BASE_DIR,'static')
     MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
