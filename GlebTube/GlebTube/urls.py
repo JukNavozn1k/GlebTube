edaa9a6f88f88d685ca698cc152ef7068a634b72
gleb <deffender1337@gmail.com>
2025-03-31 21:25:35 +0300

    Add static file serving and implement theme switching functionality in templates

--- GlebTube/GlebTube/urls.py
+++ GlebTube/GlebTube/urls.py
@@ -33,3 +33,5 @@ if DEBUG:
     
     urlpatterns += debug_toolbar_urls()
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
