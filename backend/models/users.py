0714f876a87c1e3a19969a30c0e2e1037e1f969e
gleb <deffender1337@gmail.com>
2025-08-09 22:18:50 +0300

    feat: added user in model

--- backend/models/users.py
+++ backend/models/users.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from pydantic import Field
 from beanie import Document, Indexed
 
+from typing import Optional
 
 class UserDocument(Document):
     """Пользовательская модель для MongoDB с RBAC"""
@@ -10,6 +11,12 @@ class UserDocument(Document):
   
     joined_date: datetime = Field(default_factory=datetime.utcnow)
 
+
+    # Необязательный аватар
+    avatar: Optional[str] = None  # URL или путь
+
+    # Необязательное описание, ограничим по длине
+    description: Optional[str] = Field(None, min_length=0, max_length=300)
   
     class Settings:
         name = 'users'
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,8 +1,17 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from pydantic import Field
+from typing import Optional
+
 from .base import EntityBase
 
-class UserOut(EntityBase, BaseModel):
+class UserIn(BaseModel):
     username: str
     joined_date: datetime
+
+    avatar: Optional[str] = None  
+    description: Optional[str] = Field(None, min_length=1, max_length=300)
+
+class UserOut(EntityBase, UserIn):
+    ...
