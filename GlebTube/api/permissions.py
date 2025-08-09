b417ec2d51264302e5839866b67a6e1b25cc4036
gleb <deffender1337@gmail.com>
2025-08-09 16:06:22 +0300

    refactor: removed old django app

--- GlebTube/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-class EditUserPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return bool((request.user == obj or request.user.is_staff) or request.method in SAFE_METHODS)
-
-class EditContentPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return bool((request.user == obj.author or request.user.is_staff) or request.method in SAFE_METHODS)
