d8563a626429abebd7c25ae65cddf5d7cab33b58
gleb <deffender1337@gmail.com>
2025-08-13 19:53:31 +0300

    feat: added more powerful model

--- GlebTube/ml/models.py
+++ GlebTube/ml/models.py
@@ -3,7 +3,9 @@ try:
     tokenizer = AutoTokenizer.from_pretrained("ml/models/tokenizer")
     model = AutoModel.from_pretrained("ml/models/encoder")
 except:
-    tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-mpnet-base-v2")
-    model = AutoModel.from_pretrained("sentence-transformers/all-mpnet-base-v2")
+    # tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-mpnet-base-v2")
+    # model = AutoModel.from_pretrained("sentence-transformers/all-mpnet-base-v2")
+    tokenizer = AutoTokenizer.from_pretrained("intfloat/multilingual-e5-large")
+    model = AutoModel.from_pretrained("intfloat/multilingual-e5-large")
     tokenizer.save_pretrained("ml/models/tokenizer")
     model.save_pretrained("ml/models/encoder")
